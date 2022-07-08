e9ce7f14cf41d21c3a5c688e3ff99df7186ccbee
AryanCG <108571271+AryanCG@users.noreply.github.com>
2022-07-08 15:51:45 +0530

    Add files via upload

---
+++ PickUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycasthittimg : MonoBehaviour {
+
+	float Range = 100f;
+	public Camera camera;
+	private Rigidbody rb;
+	private Collider c;
+	public LayerMask mask;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+			position();
+        }
+
+		if (Input.GetKeyDown(KeyCode.R))
+        {
+			rb.isKinematic = false;
+			c.isTrigger = false;
+			rb.transform.parent = null;
+			
+		}
+		
+	}
+
+	void position()
+    {
+		RaycastHit hit;
+		if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit , Range , mask ) )
+        {
+			rb = hit.rigidbody;
+			c = hit.collider;
+
+			rb.isKinematic = true;
+			c.isTrigger = true;
+
+			rb.transform.position = hand.transform.position;
+			rb.transform.parent = hand.transform;
+
+			
+        }
+	}
+}
