b42018773d6f6357baa8c98a9bdeca5958dc9ed9
AryanCG <108571271+AryanCG@users.noreply.github.com>
2022-07-08 17:36:22 +0530

    Update PickUp.cs

--- PickUp.cs
+++ PickUp.cs
@@ -2,23 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class raycasthittimg : MonoBehaviour {
-
+public class PickUp : MonoBehaviour {
+	 //Note all these variables are explained
 	float Range = 100f;
+	//you have to drop you fps cam in the variable
 	public Camera camera;
 	private Rigidbody rb;
 	private Collider c;
+	//make a layer mask that is Pickup layer name it
 	public LayerMask mask;
+	//create the Hand GameObject in you fps player 
+	//note Hand should be parent of fpsplayer
+	//note GameObject that you have to pickup should be in PickUp layer that you created
+	public GameObject hand;
+	//note the gameobject you are going to pick up should have rigidbody
 
 	
 	// Update is called once per frame
 	void Update () {
 
+		//PickUp item with the E on keyboard
+		
 		if (Input.GetKeyDown(KeyCode.E))
         {
 			position();
         }
 
+		//to drop the game object
 		if (Input.GetKeyDown(KeyCode.R))
         {
 			rb.isKinematic = false;
@@ -32,6 +42,7 @@ public class raycasthittimg : MonoBehaviour {
 	void position()
     {
 		RaycastHit hit;
+		//ejecting ray from fps camera
 		if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit , Range , mask ) )
         {
 			rb = hit.rigidbody;
